0cc85b7e343102a0393d5318adcdd61538d495e9
Unknown <www.limsp95@gmail.com>
2020-10-14 02:15:40 +0800

    test

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -4,64 +4,25 @@
  * @author Chris Ferdinandi
  * @license MIT
  */
-if (!Element.prototype.requestFullscreen) {
-  Element.prototype.requestFullscreen = Element.prototype.mozRequestFullscreen || Element.prototype.webkitRequestFullscreen || Element.prototype.msRequestFullscreen;
-}
-
-/**
- * document.exitFullScreen() polyfill
- * @author Chris Ferdinandi
- * @license MIT
- */
-if (!document.exitFullscreen) {
-  document.exitFullscreen = document.mozExitFullscreen || document.webkitExitFullscreen || document.msExitFullscreen;
-}
-
-function myFunction(x) {
-
-  if (document.fullscreenElement) {
-    $("#hide").show();
-    document.exitFullscreen();
-    // x.classList.toggle("fa-eye-slash");
-    $(".fa-chevron-circle-down").hide();
-    
-  } else {
-  }
-
-}
-
-/**
- * document.fullscreenElement polyfill
- * Adapted from https://shaka-player-demo.appspot.com/docs/api/lib_polyfill_fullscreen.js.html
- * @author Chris Ferdinandi
- * @license MIT
- */
-if (!document.fullscreenElement) {
-
-  Object.defineProperty(document, 'fullscreenElement', {
-    get: function() {
-      return document.mozFullScreenElement || document.msFullscreenElement || document.webkitFullscreenElement;
+!Element['prototype']['requestFullscreen'] && (Element['prototype']['requestFullscreen'] = Element['prototype']['mozRequestFullscreen'] || Element['prototype']['webkitRequestFullscreen'] || Element['prototype']['msRequestFullscreen']);
+!document['exitFullscreen'] && (document['exitFullscreen'] = document['mozExitFullscreen'] || document['webkitExitFullscreen'] || document['msExitFullscreen']);
+function myFunction(a) {
+    if (document['fullscreenElement'])
+        $('#hide')['show'](), document['exitFullscreen'](), $('.fa-chevron-circle-down')['hide']();
+    else {
     }
-  });
-
-  Object.defineProperty(document, 'fullscreenEnabled', {
-    get: function() {
-      return document.mozFullScreenEnabled || document.msFullscreenEnabled || document.webkitFullscreenEnabled;
-    }
-  });
 }
-
-document.addEventListener('click', function (event) {
-
-  // Ignore clicks that weren't on the toggle button
-  if (!event.target.hasAttribute('data-toggle-fullscreen')) return;
-
-  // If there's an element in fullscreen, exit
-  // Otherwise, enter it
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    document.documentElement.requestFullscreen();
-  }
-
-}, false);
+!document['fullscreenElement'] && (Object['defineProperty'](document, 'fullscreenElement', {
+    'get': function () {
+        return document['mozFullScreenElement'] || document['msFullscreenElement'] || document['webkitFullscreenElement'];
+    }
+}), Object['defineProperty'](document, 'fullscreenEnabled', {
+    'get': function () {
+        return document['mozFullScreenEnabled'] || document['msFullscreenEnabled'] || document['webkitFullscreenEnabled'];
+    }
+}));
+document['addEventListener']('click', function (a) {
+    if (!a['target']['hasAttribute']('data-toggle-fullscreen'))
+        return;
+    document['fullscreenElement'] ? document['exitFullscreen']() : document['documentElement']['requestFullscreen']();
+}, ![]);
