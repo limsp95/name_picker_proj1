465fcb09ad14541854fc9ae189778c63732dacf1
Unknown <www.limsp95@gmail.com>
2020-10-14 12:32:24 +0800

    fixed the scrolling bug, changed to white background and black font

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -4,6 +4,7 @@
  * @author Chris Ferdinandi
  * @license MIT
  */
+<<<<<<< obf
 !Element['prototype']['requestFullscreen'] && (Element['prototype']['requestFullscreen'] = Element['prototype']['mozRequestFullscreen'] || Element['prototype']['webkitRequestFullscreen'] || Element['prototype']['msRequestFullscreen']);
 !document['exitFullscreen'] && (document['exitFullscreen'] = document['mozExitFullscreen'] || document['webkitExitFullscreen'] || document['msExitFullscreen']);
 function myFunction(a) {
@@ -11,6 +12,33 @@ function myFunction(a) {
         $('#hide')['show'](), document['exitFullscreen'](), $('.fa-chevron-circle-down')['hide']();
     else {
     }
+=======
+if (!Element.prototype.requestFullscreen) {
+  Element.prototype.requestFullscreen = Element.prototype.mozRequestFullscreen || Element.prototype.webkitRequestFullscreen || Element.prototype.msRequestFullscreen;
+}
+
+/**
+ * document.exitFullScreen() polyfill
+ * @author Chris Ferdinandi
+ * @license MIT
+ */
+if (!document.exitFullscreen) {
+  document.exitFullscreen = document.mozExitFullscreen || document.webkitExitFullscreen || document.msExitFullscreen;
+}
+
+function myFunction(x) {
+
+  if (document.fullscreenElement) {
+    $("#hide").show();
+    document.exitFullscreen();
+    // x.classList.toggle("fa-eye-slash");
+    $(".fa-chevron-circle-down").hide();
+    $('body').css({"overflow-y": "auto"});
+    
+  } else {
+  }
+
+>>>>>>> fixed the scrolling bug, changed to white background and black font
 }
 !document['fullscreenElement'] && (Object['defineProperty'](document, 'fullscreenElement', {
     'get': function () {
